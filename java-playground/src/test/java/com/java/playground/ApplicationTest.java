2f3653eb5a866a69c7cf877f95521e278cc4ab2c
Min Huang <min.huang@alumni.usc.edu>
2021-02-20 14:54:31 -0800

    Adds a test

---
+++ java-playground/src/test/java/com/java/playground/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.java.playground;
+
+import org.junit.jupiter.api.Test;
+
+
+public final class ApplicationTest {
+  @Test
+  public void testApplication() {
+    Application application = new Application();
+  }
+}
