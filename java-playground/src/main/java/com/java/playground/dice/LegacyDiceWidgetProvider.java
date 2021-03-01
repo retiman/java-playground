f04fe353f94f43e38bc8b47046e098fde9b49d5c
Min Huang <min.huang@alumni.usc.edu>
2021-02-28 17:38:14 -0800

    Do not do this in Guice

--- java-playground/src/main/java/com/java/playground/dice/LegacyDiceWidgetProvider.java
+++ java-playground/src/main/java/com/java/playground/dice/LegacyDiceWidgetProvider.java
@@ -4,8 +4,19 @@ import javax.inject.Provider;
 
 
 final class LegacyDiceWidgetProvider implements Provider<LegacyDiceWidget> {
+  // Don't do this; this is wrong.
+  // private static LegacyDiceWidget instance;
+
   @Override
   public LegacyDiceWidget get() {
+    // This is WRONG.  Do not do this!  Allow the binding in the module to control scope.  Do not control this in the
+    // provider.
+    // if (instance == null) {
+    //   instance = LegacyDiceWidget.create();
+    // }
+    //
+    // return instance;
+
     return LegacyDiceWidget.create();
   }
 }
